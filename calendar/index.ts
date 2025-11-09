63ecd74339a6693b15aba688a1f4029f4d1410c6
Danila Volkov <noxlovette@proton.me>
2025-11-09 12:23:02 +0300

    refactor: automatic indices

--- calendar/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CalendarGrid } from "./CalendarGrid.svelte";
-export { default as Optional } from "./Optional.svelte";
-export { default as RRuleDisplay } from "./RRuleDisplay.svelte";
-export { default as RRuleSelector } from "./RRuleSelector.svelte";
