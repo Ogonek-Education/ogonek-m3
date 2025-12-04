1d7b32b3bf933fa765560604ce4cf96df0bae4d4
Danila Volkov <noxlovette@proton.me>
2025-12-04 12:25:46 +0300

    feat: navbar

---
+++ nav/navbar/Navbar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { navbar } from "./theme";
+
+  const { children } = $props();
+
+  const { base } = $derived(navbar());
+</script>
+
+<div class={base()}>
+  {@render children()}
+</div>
---
+++ nav/navbar/theme.ts
@@ -0,0 +1,33 @@
+import { tv } from "tailwind-variants";
+
+export const navbar = tv({
+  slots: {
+    base: "shadow-elevation-2 bg-md-sys-color-surface-container z-20 flex h-16 fixed bottom-0 w-full justify-around py-1.5",
+    items: "",
+  },
+});
+
+// not used
+export const navitem = tv({
+  slots: {
+    base: "justify-center flex flex-col gap-1",
+    content: "items-center flex",
+    icon: "size-6",
+    iconContainer: "relative rounded-full py-1.5 px-4",
+    label: "md-sys-typescale-label-medium",
+  },
+  variants: {
+    active: {
+      true: {
+        content: "text-md-sys-color-on-secondary-container",
+        label: "font-semibold ",
+        iconContainer:
+          "group-hover:text-md-sys-color-secondary  bg-md-sys-color-secondary-container",
+      },
+      false: {
+        content: "text-md-sys-color-on-surface-variant",
+        iconContainer: "",
+      },
+    },
+  },
+});
