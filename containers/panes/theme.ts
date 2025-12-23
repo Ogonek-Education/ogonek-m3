fc2f46f27e7343db91e2a5e066a8f8bfebd2c136
Danila Volkov <noxlovette@proton.me>
2025-12-23 18:51:02 +0300

    fix: pane margins

--- containers/panes/theme.ts
+++ containers/panes/theme.ts
@@ -8,7 +8,7 @@ export const splitPane = tv({
     left: "md:fixed top-0 bottom-0 overflow-y-auto bg-md-sys-color-surface-container overflow-x-visible py-4",
     right:
       "flex flex-col bg-md-sys-color-surface box-border min-h-[calc(100dvh-30px)] rounded-lg p-6",
-    base: "",
+    base: "pl-4",
   },
   variants: {
     centered: {
@@ -24,8 +24,8 @@ export const splitPane = tv({
 
 export const singlePane = tv({
   slots: {
-    base: "min-h-[calc(100dvh-30px)] bg-md-sys-color-surface-container  p-4 flex flex-col box-border ",
-    content: "flex flex-1 flex-col md:py-2 w-full",
+    base: "min-h-[calc(100dvh-30px)] bg-md-sys-color-surface-container p-6 flex flex-col box-border ",
+    content: "flex flex-1 flex-col md:py-2 gap-3 w-full",
     headline: "md-sys-typescale-title-medium font-serif mb-4",
   },
   variants: {
