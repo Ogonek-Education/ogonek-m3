f8d2fe58b711b6a0bb29a89509cff08ebce80529
Danila Volkov <noxlovette@proton.me>
2026-01-03 17:52:32 +0300

    feat: log in with yandex

--- containers/divider/Divider.svelte
+++ containers/divider/Divider.svelte
@@ -5,12 +5,33 @@
 
   let {
     class: className,
+    children,
     variant = "full",
     orientation = "horizontal",
     ...restProps
   }: HrProps = $props();
 
   let styling = $derived(hr({ variant, orientation }));
+  let lineClass = $derived(
+    clsx(
+      "bg-md-sys-color-outline-variant transition-colors",
+      orientation === "horizontal" ? "h-[1px] w-full" : "w-[1px] h-full"
+    )
+  );
+  let insetClass = $derived(variant === "inset" ? "mr-2 ml-4" : "");
 </script>
 
-<div {...restProps} class={clsx(styling, className)}></div>
+{#if children && orientation === "horizontal"}
+  <div
+    {...restProps}
+    class={clsx("flex w-full items-center gap-3", insetClass, className)}
+  >
+    <div class={lineClass}></div>
+    <span class="md-sys-typescale-label-small text-md-sys-color-on-surface-variant whitespace-nowrap">
+      {@render children?.()}
+    </span>
+    <div class={lineClass}></div>
+  </div>
+{:else}
+  <div {...restProps} class={clsx(styling, className)}></div>
+{/if}
