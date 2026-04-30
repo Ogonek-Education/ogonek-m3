b385e369b4908649a672ca07846a2b0d08b9e229
Danila Volkov <danila.volkov@noxlovette.com>
2026-04-30 22:28:21 +0300

    refactor: slider

--- src/lib/components/forms/slider/Slider.svelte
+++ src/lib/components/forms/slider/Slider.svelte
@@ -6,13 +6,14 @@
 	 *
 	 * @see https://m3.material.io/components/sliders/overview
 	 */
+	import { Slider } from 'bits-ui';
 	import clsx from 'clsx';
 	import { Icon } from '$lib/utils/index.js';
 	import { slider } from './theme.js';
 	import type { SliderProps } from './types.js';
 
 	let {
-		value = $bindable(),
+		value = $bindable(0),
 		min = 0,
 		max = 100,
 		step = 'any',
@@ -33,7 +34,6 @@
 
 	const {
 		root,
-		input,
 		leadingIcon,
 		leadingIconPop,
 		trailingIcon,
@@ -51,14 +51,8 @@
 	let offsetHeight = $state(600);
 	let inlineSize = $derived(vertical ? offsetHeight : offsetWidth);
 
-	const updateValue = (e: Event & { currentTarget: EventTarget & HTMLInputElement }) => {
-		const newValue = Number(e.currentTarget.value);
-		e.preventDefault();
-		value = newValue;
-	};
-
 	const range = $derived(max - min);
-	const handlePosition = $derived((value - min) / range);
+	const handlePosition = $derived((value - min) / (range || 1));
 	const stopList = $derived.by(() => {
 		const output: number[] = [];
 		const add = (stopValue: number) => {
@@ -67,7 +61,7 @@
 			output.push(stopValue);
 		};
 
-		if (stops && typeof step == 'number') {
+		if (stops && typeof step == 'number' && range > 0) {
 			for (let i = 0; i <= range; i += step) add(i / range);
 		}
 		if (endStops && !output.includes(1)) {
@@ -76,102 +70,117 @@
 
 		return output;
 	});
+
+	const stepValue = $derived(typeof step === 'number' ? step : undefined);
 </script>
 
-<div
-	class={root({ class: clsx(className) })}
-	style:--handle={handlePosition - 0.5}
-	bind:offsetWidth
-	bind:offsetHeight
+<Slider.Root
+	type="single"
+	bind:value
+	{min}
+	{max}
+	step={stepValue}
+	{disabled}
+	orientation={vertical ? 'vertical' : 'horizontal'}
+	{...extra}
 >
-	<input
-		type="range"
-		oninput={updateValue}
-		{value}
-		{min}
-		{max}
-		{step}
-		{disabled}
-		{...extra}
-		class={input({ class: clsx(vertical && 'translate-x-0 translate-y-1/2') })}
-	/>
-	<div
-		class={trackFill({
-			class: clsx(
-				vertical &&
-					'[clip-path:inset(calc(100%-var(--handle-left))_0_0_0_round_var(--m3-slider-track-in-shape)_var(--m3-slider-track-in-shape)_var(--track-radius)_var(--track-radius))]'
-			)
-		})}
-	>
-		{#each stopList as stop}
-			<div
-				class={clsx(
-					stopBase(),
-					stopFill(),
-					vertical ? '-translate-x-1/2 translate-y-1/2' : '-translate-x-1/2 -translate-y-1/2'
-				)}
-				style:--x={stop - 0.5}
-			></div>
-		{/each}
-	</div>
-	<div
-		class={trackRest({
-			class: clsx(
-				vertical &&
-					'[clip-path:inset(0_0_var(--handle-right)_0_round_var(--track-radius)_var(--track-radius)_var(--m3-slider-track-in-shape)_var(--m3-slider-track-in-shape))]'
-			)
-		})}
-	>
-		{#each stopList as stop}
-			<div
-				class={clsx(
-					stopBase(),
-					stopRest(),
-					vertical ? '-translate-x-1/2 translate-y-1/2' : '-translate-x-1/2 -translate-y-1/2'
-				)}
-				style:--x={stop - 0.5}
-			></div>
-		{/each}
-	</div>
-	{#if leadingIconProps}
-		<Icon
-			class={leadingIcon({
-				class: clsx(
-					leadingIconProps.class,
-					inlineSize * handlePosition < (size == 'xl' ? 48 : 40) && leadingIconPop(),
-					vertical && 'translate-x-[-50%] translate-y-0'
-				)
-			})}
-			{...leadingIconProps}
-		/>
-	{/if}
-	{#if trailingIconProps}
-		<Icon
-			class={trailingIcon({
-				class: clsx(
-					trailingIconProps.class,
-					inlineSize * (1 - handlePosition) < (size == 'xl' ? 48 : 40) && trailingIconPop(),
-					vertical && 'translate-x-[-50%] translate-y-0'
-				)
-			})}
-			{...trailingIconProps}
-		/>
-	{/if}
-	<div
-		class={handleCls({
-			class: clsx(vertical ? 'translate-y-1/2' : '-translate-x-1/2')
-		})}
-	></div>
-	{#if showValue}
+	{#snippet child({ props })}
 		<div
-			class={valueCls({
-				class: clsx(vertical ? 'translate-x-0 translate-y-1/2 rotate-90' : '-translate-x-1/2')
-			})}
+			{...props}
+			class={root({ class: clsx(className) })}
+			style="--handle: {handlePosition - 0.5}; {props.style ?? ''}"
 		>
-			{format(value)}
+			<div
+				class="pointer-events-none absolute inset-0"
+				bind:offsetWidth
+				bind:offsetHeight
+				aria-hidden="true"
+			></div>
+
+			<div
+				class={trackFill({
+					class: clsx(
+						vertical &&
+							'[clip-path:inset(calc(100%-var(--handle-left))_0_0_0_round_var(--m3-slider-track-in-shape)_var(--m3-slider-track-in-shape)_var(--track-radius)_var(--track-radius))]'
+					)
+				})}
+			>
+				{#each stopList as stop}
+					<div
+						class={clsx(
+							stopBase(),
+							stopFill(),
+							vertical ? '-translate-x-1/2 translate-y-1/2' : '-translate-x-1/2 -translate-y-1/2'
+						)}
+						style:--x={stop - 0.5}
+					></div>
+				{/each}
+			</div>
+
+			<div
+				class={trackRest({
+					class: clsx(
+						vertical &&
+							'[clip-path:inset(0_0_var(--handle-right)_0_round_var(--track-radius)_var(--track-radius)_var(--m3-slider-track-in-shape)_var(--m3-slider-track-in-shape))]'
+					)
+				})}
+			>
+				{#each stopList as stop}
+					<div
+						class={clsx(
+							stopBase(),
+							stopRest(),
+							vertical ? '-translate-x-1/2 translate-y-1/2' : '-translate-x-1/2 -translate-y-1/2'
+						)}
+						style:--x={stop - 0.5}
+					></div>
+				{/each}
+			</div>
+
+			{#if leadingIconProps}
+				<Icon
+					class={leadingIcon({
+						class: clsx(
+							leadingIconProps.class,
+							inlineSize * handlePosition < (size == 'xl' ? 48 : 40) && leadingIconPop(),
+							vertical && 'translate-x-[-50%] translate-y-0'
+						)
+					})}
+					{...leadingIconProps}
+				/>
+			{/if}
+			{#if trailingIconProps}
+				<Icon
+					class={trailingIcon({
+						class: clsx(
+							trailingIconProps.class,
+							inlineSize * (1 - handlePosition) < (size == 'xl' ? 48 : 40) && trailingIconPop(),
+							vertical && 'translate-x-[-50%] translate-y-0'
+						)
+					})}
+					{...trailingIconProps}
+				/>
+			{/if}
+
+			<Slider.Thumb
+				index={0}
+				class={handleCls({
+					class: clsx(vertical ? 'translate-y-1/2' : '-translate-x-1/2')
+				})}
+			/>
+
+			{#if showValue}
+				<div
+					class={valueCls({
+						class: clsx(vertical ? 'translate-x-0 translate-y-1/2 rotate-90' : '-translate-x-1/2')
+					})}
+				>
+					{format(value)}
+				</div>
+			{/if}
 		</div>
-	{/if}
-</div>
+	{/snippet}
+</Slider.Root>
 
 <style>
 	@layer tokens {
--- src/lib/components/forms/slider/theme.ts
+++ src/lib/components/forms/slider/theme.ts
@@ -2,7 +2,7 @@ import { tv, type VariantProps } from 'tailwind-variants';
 
 export const slider = tv({
 	slots: {
-		root: 'slider-root relative min-w-40 [block-size:var(--handle-height)] [print-color-adjust:exact] [--m3-slider-track-out-shape:0.5rem] [--m3-slider-track-in-shape:0.125rem] [--m3-slider-handle-shape:var(--m3-shape-full)] [--functional-width:calc(100%-2*(0.25rem+0.125rem))] [--handle-left:calc(50%+var(--functional-width)*var(--handle)-0.125rem-0.375rem)] [--handle-center:calc(50%+var(--functional-width)*var(--handle))] [--handle-right:calc(50%+var(--functional-width)*var(--handle)+0.125rem+0.375rem)]',
+		root: 'slider-root group relative block min-w-40 [block-size:var(--handle-height)] [print-color-adjust:exact] [--m3-slider-track-out-shape:0.5rem] [--m3-slider-track-in-shape:0.125rem] [--m3-slider-handle-shape:var(--m3-shape-full)] [--functional-width:calc(100%-2*(0.25rem+0.125rem))] [--handle-left:calc(50%+var(--functional-width)*var(--handle)-0.125rem-0.375rem)] [--handle-center:calc(50%+var(--functional-width)*var(--handle))] [--handle-right:calc(50%+var(--functional-width)*var(--handle)+0.125rem+0.375rem)] cursor-pointer data-[disabled]:cursor-default',
 		input:
 			'peer absolute left-1/2 [inline-size:calc(var(--functional-width)+1rem)] [block-size:100%] -translate-x-1/2 appearance-none opacity-0 m-0 enabled:cursor-pointer',
 		leadingIcon:
@@ -12,18 +12,18 @@ export const slider = tv({
 			'absolute top-1/2 right-1 h-[var(--icon-size)] w-[var(--icon-size)] -translate-y-1/2 text-[var(--m3c-primary)] pointer-events-none',
 		trailingIconPop: 'right-[calc(100%-var(--handle-left))] text-[var(--m3c-secondary-container)]',
 		trackFill:
-			'absolute inset-x-0 [inset-block:calc((var(--handle-height)-var(--track-height))/2)] pointer-events-none bg-[var(--m3c-primary)] [clip-path:inset(0_calc(100%-var(--handle-left))_0_0_round_var(--track-radius)_var(--m3-slider-track-in-shape)_var(--m3-slider-track-in-shape)_var(--track-radius))] peer-disabled:bg-[--translucent(var(--m3c-on-surface),0.38)] [@media(forced-colors:active)]:bg-[selecteditem] peer-disabled:[@media(forced-colors:active)]:bg-[canvastext]',
+			'absolute inset-x-0 [inset-block:calc((var(--handle-height)-var(--track-height))/2)] pointer-events-none bg-[var(--m3c-primary)] [clip-path:inset(0_calc(100%-var(--handle-left))_0_0_round_var(--track-radius)_var(--m3-slider-track-in-shape)_var(--m3-slider-track-in-shape)_var(--track-radius))] group-data-[disabled]:bg-[--translucent(var(--m3c-on-surface),0.38)] [@media(forced-colors:active)]:bg-[selecteditem] group-data-[disabled]:[@media(forced-colors:active)]:bg-[canvastext]',
 		trackRest:
-			'absolute inset-x-0 [inset-block:calc((var(--handle-height)-var(--track-height))/2)] pointer-events-none bg-[var(--m3c-secondary-container)] [clip-path:inset(0_0_0_var(--handle-right)_round_var(--m3-slider-track-in-shape)_var(--track-radius)_var(--track-radius)_var(--m3-slider-track-in-shape))] peer-disabled:bg-[--translucent(var(--m3c-on-surface),0.12)] [@media(forced-colors:active)]:bg-[canvastext] peer-disabled:[@media(forced-colors:active)]:bg-[graytext]',
+			'absolute inset-x-0 [inset-block:calc((var(--handle-height)-var(--track-height))/2)] pointer-events-none bg-[var(--m3c-secondary-container)] [clip-path:inset(0_0_0_var(--handle-right)_round_var(--m3-slider-track-in-shape)_var(--track-radius)_var(--track-radius)_var(--m3-slider-track-in-shape))] group-data-[disabled]:bg-[--translucent(var(--m3c-on-surface),0.12)] [@media(forced-colors:active)]:bg-[canvastext] group-data-[disabled]:[@media(forced-colors:active)]:bg-[graytext]',
 		stopBase:
 			'absolute top-1/2 h-1 w-1 rounded-full [inset-inline-start:calc(50%+(100%-0.5rem-0.25rem)*var(--x))] pointer-events-none',
-		stopFill: 'bg-[var(--m3c-on-primary)] peer-disabled:bg-[var(--m3c-inverse-on-surface)]',
+		stopFill: 'bg-[var(--m3c-on-primary)] group-data-[disabled]:bg-[var(--m3c-inverse-on-surface)]',
 		stopRest:
-			'bg-[var(--m3c-primary)] peer-disabled:bg-[--translucent(var(--m3c-on-surface),0.38)]',
+			'bg-[var(--m3c-primary)] group-data-[disabled]:bg-[--translucent(var(--m3c-on-surface),0.38)]',
 		handle:
-			'absolute inset-y-0 left-[var(--handle-center)] w-1 rounded-[1.25rem] bg-[var(--m3c-primary)] pointer-events-none peer-focus-visible:outline peer-focus-visible:outline-[4px] peer-focus-visible:outline-[var(--m3c-on-secondary-container)] peer-focus-visible:outline-offset-[5px] peer-focus-visible:z-[2] peer-focus-visible:w-0.5 peer-active:w-0.5 peer-disabled:bg-[--translucent(var(--m3c-on-surface),0.38)] [@media(forced-colors:active)]:bg-[selecteditem] peer-disabled:[@media(forced-colors:active)]:bg-[graytext]',
+			'absolute inset-y-0 left-[var(--handle-center)] w-1 rounded-[1.25rem] bg-[var(--m3c-primary)] pointer-events-none group-focus-within:outline group-focus-within:outline-[4px] group-focus-within:outline-[var(--m3c-on-secondary-container)] group-focus-within:outline-offset-[5px] group-focus-within:z-[2] group-focus-within:w-0.5 group-active:w-0.5 group-data-[disabled]:bg-[--translucent(var(--m3c-on-surface),0.38)] [@media(forced-colors:active)]:bg-[selecteditem] group-data-[disabled]:[@media(forced-colors:active)]:bg-[graytext]',
 		value:
-			'absolute flex min-w-12 items-center justify-center rounded-[var(--m3-slider-handle-shape)] bg-[var(--m3c-inverse-surface)] px-4 py-3 text-[var(--m3c-inverse-on-surface)] md-sys-typescale-label-large [inset-inline-start:var(--handle-center)] [inset-block-end:calc(var(--handle-height)+4px)] opacity-0 pointer-events-none select-none z-[1] peer-hover:opacity-100 peer-focus-visible:opacity-100 peer-active:opacity-100 [@media(forced-colors:active)]:border-2 [@media(forced-colors:active)]:border-[selecteditem] [@media(forced-colors:active)]:overflow-hidden'
+			'absolute flex min-w-12 items-center justify-center rounded-[var(--m3-slider-handle-shape)] bg-[var(--m3c-inverse-surface)] px-4 py-3 text-[var(--m3c-inverse-on-surface)] md-sys-typescale-label-large [inset-inline-start:var(--handle-center)] [inset-block-end:calc(var(--handle-height)+4px)] opacity-0 pointer-events-none select-none z-[1] group-hover:opacity-100 group-focus-within:opacity-100 group-active:opacity-100 [@media(forced-colors:active)]:border-2 [@media(forced-colors:active)]:border-[selecteditem] [@media(forced-colors:active)]:overflow-hidden'
 	},
 	variants: {
 		size: {
